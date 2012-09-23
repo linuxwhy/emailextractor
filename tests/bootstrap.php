363330430c5c55aac4e58100214c8f5b3440af74
oaki <oaki@192.168.1.2>
2012-09-23 21:30:30 +0200

    selenium

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,35 @@
 <?php
 
 define('WWW_DIR', __DIR__);
+
 define('APP_DIR', WWW_DIR . '/../app');
 define('LIBS_DIR', WWW_DIR . '/../libs');
 define('VENDOR_DIR', WWW_DIR . '/../vendor');
 define('DATA_DIR', WWW_DIR . '/../data');
 
-require APP_DIR . '/bootstrap.php';
+require VENDOR_DIR.'/nette/nette/Nette/loader.php';
+
+// Configure application
+$configurator = new Nette\Config\Configurator;
+
+// Enable Nette Debugger for error visualisation & logging
+//$configurator->setDebugMode($configurator::AUTO);
+$configurator->enableDebugger(WWW_DIR . '/../log');
+
+// Enable RobotLoader - this will load all classes automatically
+
+$configurator->setTempDirectory(WWW_DIR . '/../temp');
+$configurator->createRobotLoader()
+	->addDirectory(APP_DIR)
+	->addDirectory(LIBS_DIR)
+	->addDirectory(VENDOR_DIR.'/dg/dibi/dibi')
+	->register();
+
+// Create Dependency Injection container from config.neon file
+$configurator->addConfig(APP_DIR . '/config.neon');
+
+$container = $configurator->createContainer();
+
 
 require WWW_DIR . '/UnitTestCase.php';
 
